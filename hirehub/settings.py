de270d3acb923fd1b6b9d35f78f7394fa678f60e
Leena Jain <codewithleena@gmail.com>
2026-05-27 18:56:34 +0530

    Fixed database config for Render

--- hirehub/settings.py
+++ hirehub/settings.py
@@ -73,11 +73,11 @@ WSGI_APPLICATION = 'hirehub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hirehub',
-        'USER': 'leenajain',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
