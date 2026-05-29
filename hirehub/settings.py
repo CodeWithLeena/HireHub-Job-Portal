9d9b9ff4ced3d91e5a07f66036abe97792a6a28d
Leena Jain <codewithleena@gmail.com>
2026-05-29 12:24:32 +0530

    added seed data fix

--- hirehub/settings.py
+++ hirehub/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'hirehub.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
